55d1c72bfaea0936313c00e51969490654ebac8b
void <skeletonvoid0@gmail.com>
2026-02-15 19:50:25 +0800

    copied README to project root directory

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ rm jiexi-unstubbed.wasm jiexi.wasm
 cp -T "$(wasm32-wasi-cabal list-bin jiexi)" ./jiexi-unstubbed.wasm
 wasi-stub jiexi-unstubbed.wasm -o jiexi.wasm --return-value 0
 cp jiexi.wasm packages/jiexi
+cp README.md packages/jiexi
